7a867eafe6cd6c5f043e2d2a2d931cd24855b445
Evgeniy <zhenyakurilo@gmail.com>
2017-12-05 01:04:43 +0200

    step 2: material ui

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import {Card, CardActions, FlatButton} from 'material-ui';
 
 export const Contact = props => (
-  <div>
+  <Card>
     <div>First Name: {props.contact.firstName}</div>
     <div>Last Name: {props.contact.lastName}</div>
     <div>Phone: {props.contact.phone}</div>
-  </div>
+    <CardActions>
+      <FlatButton primary={true}>Edit</FlatButton>
+      <FlatButton secondary={true}>Delete</FlatButton>
+    </CardActions>
+  </Card>
 );
--- src/ContactList.js
+++ src/ContactList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Contact} from './Contact';
+import {FlatButton, GridList} from 'material-ui';
 
 export default class ContactList extends Component {
 
@@ -10,15 +11,19 @@ export default class ContactList extends Component {
   componentDidMount() {
     fetch('http://localhost:8090/api/contacts')
       .then(resp => resp.json())
-      .then(json => this.setState({contacts: json._embedded.contacts}))
+      .then(json => this.setState({contacts: json._embedded.contacts}));
   }
 
   render() {
     let contacts = this.state.contacts.map(contact => <Contact key={contact.id} contact={contact}/>);
     return (
       <div>
-        {contacts}
+        <FlatButton primary={true}>Add</FlatButton>
+        <GridList cols={3}>
+          {contacts}
+        </GridList>
       </div>
+
     );
 
   }
